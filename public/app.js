3271d984ff9250bdbc2542802f6ef33c5984715d
Aayush Dutt <aayushd100@gmail.com>
2018-01-10 13:50:00 +0000

    fixed the fixed parent url

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $( "#formInput" ).on( "submit", function( event ) {
   event.preventDefault();
    var data = $( this ).serialize() ;
   $.post("/api/", data);
-  $( "#info" ).text("Done! Access your URL at https://node-aayushd100.c9users.io/"+ $( "#givenName" ).val());
+  $( "#info" ).text("Done! Access your URL at /"+ $( "#givenName" ).val());
   $( "#givenName" ).val("");
   $( "#givenUrl" ).val("");
 });
