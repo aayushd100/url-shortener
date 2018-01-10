04c48e8f91bf6347d008773de773e7c6acab29b5
Aayush Dutt <aayushd100@gmail.com>
2018-01-10 18:07:32 +0000

    added the clickable link display in the messagebox

--- public/app.js
+++ public/app.js
@@ -1,9 +1,14 @@
 /* global $ */
+var windowLocation = window.location.href;
+$("#baseurl").text(windowLocation)
 $( "#formInput" ).on( "submit", function( event ) {
   event.preventDefault();
    var data = $( this ).serialize() ;
   $.post("/api/", data);
-  $( "#info" ).text("Done! Access your URL at /"+ $( "#givenName" ).val());
+  $( "#doneSpan" ).html('Done! Use <a id="windowLocation" href=""></a> to access the url.');
+  var finalurl = windowLocation + $( "#givenName" ).val();
+  $('#windowLocation').text(finalurl);
+  $("a").attr("href", finalurl);
   $( "#givenName" ).val("");
   $( "#givenUrl" ).val("");
 });
