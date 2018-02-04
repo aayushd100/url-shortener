009b68994646a458ed20bbb02612459fb976c25c
aayushd100 <aayushd100@gmail.com>
2018-02-04 16:32:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
 
 
 //...other requests
