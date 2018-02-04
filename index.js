99d030e0a96434b1c980cd666c4088fbd110c00c
aayushd100 <aayushd100@gmail.com>
2018-02-04 16:17:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,23 +12,24 @@ app.use(express.static(__dirname + '/views'));
 
 
 
-var urlRoutes = require('./routes/urls');
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
-
-app.use('/', urlRoutes);
-
-/*
-app.get('/', function(req,res){
-res.send("hey now brown cow!");    
-})*/
-
-
+if (req.method === 'OPTIONS') {
+      console.log('!OPTIONS');
+      var headers = {};
+      // IE8 does not allow domains to be specified, just the *
+      // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+      headers["Access-Control-Allow-Origin"] = "*";
+      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+      headers["Access-Control-Allow-Credentials"] = false;
+      headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+      res.writeHead(200, headers);
+      res.end();
+} else {
+//...other requests
+  app.use('/', urlRoutes);
+}
 
 
 
